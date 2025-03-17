d92330fb25aa709c29f1d58c0733f7bbf1e42092
Anatoly <anatolykostarev@gmail.com>
2025-03-18 00:50:11 +0300

    Add appwrite backend DB, create auth by appwrite (#2)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { account } from "~/lib/appwrite";
+import { useAuthStore, useIsLoadingStore } from "~/store/auth.store";
+
+const isLoadingStore = useIsLoadingStore();
+const authStore = useAuthStore();
+const router = useRouter();
+
+onMounted(async () => {
+  try {
+    const user = await account.get();
+    if (user) {
+      authStore.set(user);
+    } else {
+      router.push("/login");
+    }
+  } catch (error) {
+    router.push("/login");
+  } finally {
+    isLoadingStore.set(false);
+  }
+});
+</script>
 
 <template>
-  <section class="grid">
-    <LayoutSidebar />
+  <LayoutLoader v-if="isLoadingStore.isLoading" />
+  <section
+    v-else
+    :class="{ grid: authStore.isAuth }">
+    <LayoutSidebar v-if="authStore.isAuth" />
     <div>
       <slot />
     </div>
---
+++ lib/app.constants.ts
@@ -0,0 +1,7 @@
+// srm-system-nuxt-record
+export const APP_WRITE_ID = "srm-system-nuxt-record";
+export const DB_ID = "crm-base";
+export const COLLECTION_DEALS = "deals";
+export const COLLECTION_CUSTOMERS = "customers";
+export const COLLECTION_COMMENTS = "comments";
+export const STORAGE_ID = "storage";
