e7d2b3d8c7adac3baa2e5cdbd02ee28d1e4dd85a
Anatoly <anatolykostarev@gmail.com>
2025-03-18 00:49:52 +0300

    Add appwrite backend DB, create auth by appwrite

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,4 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { account } from "~/lib/appwrite";
+import { useAuthStore, useIsLoadingStore } from "~/store/auth.store";
+
+const isLoadingStore = useIsLoadingStore();
+const authStore = useAuthStore();
+const router = useRouter();
+
+const logout = async () => {
+  isLoadingStore.set(true);
+  await account.deleteSession("current");
+  authStore.clear();
+  await router.push("/login");
+  isLoadingStore.set(false);
+};
+</script>
 
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
@@ -11,7 +26,9 @@
         width="140" />
     </NuxtLink>
 
-    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+    <button
+      class="absolute top-2 right-3 transition-colors hover:text-primary"
+      @click="logout">
       <Icon
         name="line-md:logout"
         size="20" />
