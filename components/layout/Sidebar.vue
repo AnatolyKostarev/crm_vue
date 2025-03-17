1af55e4cf55f5e9183f53f225af342b6867ea9e5
Anatoly <anatolykostarev@gmail.com>
2025-03-17 22:02:54 +0300

    Initial styles, add tailwind, shadcn, create menu, sidebar, default layout

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink
+      to="/"
+      class="mb-10 block">
+      <NuxtImg
+        class="mx-auto"
+        src="/logo.svg"
+        width="140" />
+    </NuxtLink>
+
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon
+        name="line-md:logout"
+        size="20" />
+    </button>
+    <LayoutMenu />
+  </aside>
+</template>
+
+<style scoped></style>
